6513a01e9857b58be31ba0b794c07eaf3d30dd32
Andrei <kuzmenko_andrey_@mail.ru>
2023-02-19 19:17:34 +0300

    Перенес данные в контекст, добавил вызов апи заказа (#3)

--- src/utils/endpoints.js
+++ src/utils/endpoints.js
@@ -1,1 +1,2 @@
-export const ingredientAPI = 'https://norma.nomoreparties.space/api/ingredients'
+export const ingredientAPI = 'https://norma.nomoreparties.space/api/ingredients';
+export const orderAPI = 'https://norma.nomoreparties.space/api/orders';
